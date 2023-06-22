fcd132b3761993697309baf62bda018370ec0cfb
VanessaSR <103793183+vsrodriguez3@users.noreply.github.com>
2023-06-22 13:57:04 -0400

    Add function that gets computer's choice

---
+++ index.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor( Math.random() * 3);
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else if (randomNumber === 2) {
+        return "scissors";
+    }
+}
+
+getComputerChoice();
